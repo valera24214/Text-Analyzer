3f0aec7e158792809115dcf90c7ca7d9f9f87ce3
User <valera.astroukh@gmail.com>
2026-02-26 19:20:01 +0300

    CountResultArgs - ResultArgs with int. SettedCharCountProcessor implemented - class that can count how many setted letters located in string array

---
+++ WinFormsApp2/EventsArguments/CountResultArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp2.EventsArguments
+{
+    internal class CountResultArgs : ResultEventArgs
+    {
+        public int count {  get; }
+        public override string description { get; }
+
+        public CountResultArgs(int count, string description)
+        {
+            this.count = count;
+            this.description = description;
+        }
+    }
+}
---
+++ WinFormsApp2/TextProcessors/SettedCharCountProcessor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinFormsApp2.EventsArguments;
+
+namespace WinFormsApp2.TextProcessors
+{
+    internal class SettedCharCountProcessor : ITextProcessor
+    {
+        public event EventHandler<ResultEventArgs> OnProcessed;
+        private char target;
+        
+        public SettedCharCountProcessor(char target)
+        {
+            this.target = target; 
+        }
+
+        public string Process(string[] words)
+        {
+            int count = 0;
+            foreach (string word in words) 
+            {
+                count += word.Count(ch => char.ToLower(ch) == char.ToLower(target));
+            }
+
+            OnProcessed?.Invoke(this, new CountResultArgs(count, $"Количество букв {target} в тексте"));
+            return count.ToString();
+        }
+    }
+}
