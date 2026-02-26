13a64140b66a3e937d3ca96579bd0e6a81e793c8
User <valera.astroukh@gmail.com>
2026-02-26 09:38:48 +0300

    TextProder & TextProcessor interfaces defined. Created base ResultEventArgs class

---
+++ WinFormsApp2/TextProcessors/ITextProcessor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinFormsApp2.EventsArguments;
+
+namespace WinFormsApp2.TextProcessors
+{
+    internal interface ITextProcessor
+    {
+        event EventHandler<ResultEventArgs> OnProcessed;
+        string Process(string[] words);
+    }
+}
